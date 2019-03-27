485ec8649a8c1e279d9a53b013e4247e0e81a16a
mianurviaraya <47815521+mianurviaraya@users.noreply.github.com>
2019-03-27 23:18:54 +0700

    Add files via upload

---
+++ insertion sort.cpp
@@ -0,0 +1,37 @@
+//NAMA  : MIA NURVIA RAYA
+//NIM   : 18050623015
+//PRODI : D3 MANAJEMEN INFORMATIKA
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int i,b,key, j,n,arr[100];
+
+    cout<<endl<<endl;
+	cout<<"masukkan jumlah angka ";
+    cin>>n;
+    for(int t=0;t<n;t++){
+
+            cout<<"masukan angka ke- " <<t+1<<" : ";
+            cin >> arr[t];
+        }
+
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+
+    for (i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+}
---
+++ selection sort.cpp
@@ -0,0 +1,36 @@
+//NAMA  : MIA NURVIA RAYA
+//NIM   : 18050623015
+//PRODI : D3 MANAJEMEN INFORMATIKA
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,temp,arr[100];
+
+    cout<<endl<<endl;
+    cout<<"masukkan jumlah angka ";
+    cin>>b;
+    for(int j=0;j<b;j++){
+            cout<<"masukan angka ke- " <<j+1<<" : ";
+            cin >> arr[j];
+        }
+
+    for(int i=0;i<b;i++){
+        for(int j=0;j<b-1;j++){
+            if(arr[i]<arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+
+    for(a=0;a<b;a++){
+        cout<<endl;
+        cout<<arr[a];
+    }
+}
